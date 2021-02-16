2d3e88d739697559b41fa558b7c1777a22b11644
v1enna <19vienna20@gmail.com>
2021-02-16 20:16:19 +0100

    Initialize TypeScript conversion

---
+++ src/components/FailureHandler.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../sass/main.sass";
+
+export default function FailureHandler(props: { error: string }) {
+    return (
+        <div className="default-page">
+            Questa pagina non esiste.
+            <br />
+            <div>
+                <strong>Errore:</strong>{" "}
+                <span className="error-message">{props.error}</span>
+            </div>
+        </div>
+    );
+}
